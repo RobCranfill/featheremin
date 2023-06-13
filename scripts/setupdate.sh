d2a01f190ee650a6ab8677d198962d6f920d4dad
Rob Cranfill <robcranfill@robcranfill.net>
2023-06-13 15:50:03 -0700

    Create scripts dir

---
+++ scripts/setupdate.sh
@@ -0,0 +1,7 @@
+# 'source' this to create a function that will update FROM the RP2040 to here, to be committed.
+# to see it it's set, do 'type update'
+function update() {
+	CPATH=/mnt/CIRCUITPY
+	sudo mount -t drvfs F: $CPATH
+	cp -uv $CPATH/feathereminMain.py $CPATH/feathereminDisplay9341.py $CPATH/README.md .
+}
