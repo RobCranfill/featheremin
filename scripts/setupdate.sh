5b848ce685cc2699be1f2f165abbef8f651118c6
Rob Cranfill <robcranfill@gmail.com>
2023-08-19 13:26:01 -0700

    Fixed fallingforever issue (thanks todbot)

--- scripts/setupdate.sh
+++ scripts/setupdate.sh
@@ -1,9 +1,8 @@
 # 'source' this to create a function that will update FROM the RP2040 to here, to be committed.
-# to see it it's set, do 'type update'
-function update() {
+# to see it it's set, do 'type cpupdate'
+# This is for native Ubuntu (not WSL)
+function cpupdate() {
 	CPATH=/media/rob/CIRCUITPY
-# this is for WSL2 only!
-#	sudo mount -t drvfs F: $CPATH 
 	cp -uv $CPATH/feathereminMain.py $CPATH/feathereminDisplay1.py /$CPATH/feathereminDisplay2.py $CPATH/featherSynth5.py $CPATH/README.md .
 	cp -uv $CPATH/test/* ./test
 }
--- scripts/setupdateWSL.sh
+++ scripts/setupdateWSL.sh
@@ -1,9 +1,9 @@
 # 'source' this to create a function that will update FROM the RP2040 to here, to be committed.
-# to see it it's set, do 'type update'
-function update() {
-	CPATH=/mnt/CIRCUITPY
+# to see it it's set, do 'type cpupdate'
 # this is for WSL2 only!
-#	sudo mount -t drvfs F: $CPATH 
+function cpupdate() {
+	CPATH=/mnt/CIRCUITPY
+	sudo mount -t drvfs F: $CPATH 
 	cp -uv $CPATH/feathereminMain.py $CPATH/feathereminDisplay1.py /$CPATH/feathereminDisplay2.py $CPATH/featherSynth5.py $CPATH/README.md .
 	cp -uv $CPATH/test/* ./test
 }
